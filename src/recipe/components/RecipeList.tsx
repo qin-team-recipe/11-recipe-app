2738a60c14ea4fede89057b538cb42c9c47ba08e
Keitoto <keitasekihara@gmail.com>
2023-06-07 18:04:45 +0900

    ✨ RecipeList追加

---
+++ src/recipe/components/RecipeList.tsx
@@ -0,0 +1,48 @@
+import { RecipeCard } from '@/recipe/components/RecipeCard';
+import { dummyRecipeList } from '@/recipe/data/dummyRecipe';
+import { Recipe } from '@/recipe/types/Recipe';
+import React, { FC } from 'react';
+
+type Props = {
+  recipeList: Recipe[];
+  isScroll?: boolean;
+};
+
+// export const RecipeList: FC<Props> = ({ recipeList, isScroll }) => {
+//   const recipe = recipeList.length > 0 ? recipeList : dummyRecipeList;
+//   return (
+//     <ul className="grid grid-cols-2  gap-x-3 gap-y-4">
+//       {recipe.map((recipe) => (
+//         <li key={recipe.id} className="">
+//           <RecipeCard recipe={recipe} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+export const RecipeListVertical: FC<Props> = ({ recipeList }) => {
+  const recipe = recipeList.length > 0 ? recipeList : dummyRecipeList;
+  return (
+    <ul className="grid grid-cols-2  gap-x-3 gap-y-4">
+      {recipe.map((recipe) => (
+        <li key={recipe.id} className="">
+          <RecipeCard recipe={recipe} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export const RecipeListHorizontal: FC<Props> = ({ recipeList }) => {
+  const recipe = recipeList.length > 0 ? recipeList : dummyRecipeList;
+  return (
+    <ul className="flex gap-x-[4%] overflow-x-scroll hidden-scrollbar">
+      {recipe.map((recipe) => (
+        <li key={recipe.id} className="w-[44%] flex-shrink-0">
+          <RecipeCard recipe={recipe} />
+        </li>
+      ))}
+    </ul>
+  );
+};
