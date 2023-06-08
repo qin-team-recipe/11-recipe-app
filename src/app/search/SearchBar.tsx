dcf25aee1ffa47b37bf0dded5ca247283e14a21b
Keitoto <keitasekihara@gmail.com>
2023-06-08 18:55:39 +0900

    ✨Debounce機能を追加。スピナーも表示。

--- src/app/search/SearchBar.tsx
+++ src/app/search/SearchBar.tsx
@@ -1,17 +1,60 @@
 'use client';
 
-import { useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Image from 'next/image';
+import { Spinner } from '@/app/search/components/Spinner';
 
 type Props = {
   isSearchPage?: boolean;
 };
 
-export const SearchBar = ({ isSearchPage = true }) => {
-  const [searchText, setSearchText] = useState('');
+export const SearchBar: FC<Props> = ({ isSearchPage = false }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  const [isDebouncing, setIsDebouncing] = useState(false);
+
+  let debounceTimeout: NodeJS.Timeout;
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+
+    // If the search term is empty, don't debounce
+    if (value === '') return;
+
+    setIsDebouncing(true);
+    clearTimeout(debounceTimeout);
+  };
+
+  useEffect(() => {
+    if (searchTerm === '') {
+      setIsDebouncing(false);
+      setDebouncedSearchTerm('');
+      return;
+    }
+    setIsDebouncing(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    debounceTimeout = setTimeout(() => {
+      setDebouncedSearchTerm(searchTerm);
+      setIsDebouncing(false);
+    }, 1000);
+    return () => {
+      clearTimeout(debounceTimeout);
+    };
+  }, [searchTerm]);
+
+  useEffect(() => {
+    performSearch(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  const performSearch = (value: string) => {
+    console.log('Searching for:', value);
+    // Perform your search operation here
+  };
 
   return (
     <div className="w-full flex items-center gap-x-4">
+      {/* {isDebouncing && <p>Debouncing...</p>} */}
       {isSearchPage && (
         <button type="button" className="flex-shrink-0">
           <Image
@@ -24,7 +67,7 @@ export const SearchBar = ({ isSearchPage = true }) => {
       )}
       <form
         action=""
-        className="flex-1 flex bg-mauve-4 h-10 px-4 gap-x-3 font-bold rounded-xl py-1"
+        className="flex-1 flex items-center bg-mauve-4 h-10 px-4 gap-x-3 font-bold rounded-xl py-1"
       >
         {!isSearchPage && (
           <div className="flex-shrink-0 flex items-center">
@@ -38,11 +81,30 @@ export const SearchBar = ({ isSearchPage = true }) => {
         )}
         <input
           className="flex-1 bg-transparent outline-none text-mauve-12 placeholder:text-mauve-9"
-          type="search"
-          value={searchText}
+          type="text"
+          value={searchTerm}
           placeholder="シェフやレシピを検索"
-          onChange={(e) => setSearchText(e.target.value)}
+          onChange={handleSearchChange}
         />
+        <div className="w-5 h-5 flex items-center">
+          {isDebouncing ? (
+            <Spinner />
+          ) : searchTerm ? (
+            <button
+              type="button"
+              onClick={() => {
+                setSearchTerm('');
+              }}
+            >
+              <Image
+                src="/assets/icons/CloseIcon.svg"
+                alt=""
+                width={20}
+                height={20}
+              />
+            </button>
+          ) : null}
+        </div>
       </form>
     </div>
   );
