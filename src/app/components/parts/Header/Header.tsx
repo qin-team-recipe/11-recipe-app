8f65ac41fb138d9fdf80998fbb63ea60c2143745
Keitoto <keitasekihara@gmail.com>
2023-06-15 16:16:32 +0900

    ✨ 再利用可能Header追加

---
+++ src/app/components/parts/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactNode } from "react";
+
+export const Header:FC<{children:ReactNode}> = ({children}) => {
+  return (
+    <header className="w-full flex justify-center items-center h-12 border-b border-mauve-6 bg-mauve-2 font-bold text-xl leading-0">
+      {children}
+    </header>
+  )
+}
