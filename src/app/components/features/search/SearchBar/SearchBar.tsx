cf8aabf62e74a104dfd60766ef107cddcbc37eb7
Keitoto <keitasekihara@gmail.com>
2023-06-15 17:43:04 +0900

    ✨ URLにクエリを追加する機能実装

--- src/app/components/features/search/SearchBar/SearchBar.tsx
+++ src/app/components/features/search/SearchBar/SearchBar.tsx
@@ -1,72 +1,65 @@
 'use client';
 
-import { FC, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname, useRouter, useSelectedLayoutSegment } from 'next/navigation';
+
 import { Spinner } from './_Spinner';
 import { useDebounce } from './useDebounce';
 
-type Props = {
-  isSearchPage?: boolean;
-};
+export const SearchBar = () => {
+  const segment = useSelectedLayoutSegment();
+  const isSearchPage = segment === 'search';
 
-export const SearchBar: FC<Props> = ({ isSearchPage = false }) => {
+  const path = usePathname().split('/');
+  const searchType = path[path.length - 1] || 'recipe'; // recipe or chef
+
+  const router = useRouter();
+
   const [searchTerm, setSearchTerm] = useState('');
   const [debouncedSearchTerm, isDebouncing] = useDebounce(searchTerm, 1000);
   const hasTypedRef = useRef(false);
 
   useEffect(() => {
-    // TODO: 検索が空の場合に走らせるかを検討
-    // if (debouncedSearchTerm === '') return;
+    const performSearch = (value: string) => {
+      router.push(`/search/${searchType}?q=${value}`);
+    };
     if (!hasTypedRef.current) return;
     performSearch(debouncedSearchTerm);
-  }, [debouncedSearchTerm]);
+  }, [debouncedSearchTerm, searchType, router]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
     hasTypedRef.current = true;
   };
 
-  const performSearch = (value: string) => {
-    console.log('Searching for:', value);
-    // TODO: 検索処理
-  };
-
   return (
-    <div className="w-full flex items-center gap-x-4">
+    <div className="flex w-full items-center gap-x-4 border-b border-mauve-6 px-4 py-2">
       {/* 検索ページでのみ戻るボタンを表示 */}
       {isSearchPage && (
-        <button type="button" className="flex-shrink-0">
-          <Image
-            src="/assets/icons/BackIcon-dark.svg"
-            alt="戻る"
-            width={20}
-            height={20}
-          />
-        </button>
+        <Link href="/" className="flex-shrink-0">
+          <Image src="/assets/icons/BackIcon-dark.svg" alt="戻る" width={20} height={20} />
+        </Link>
       )}
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="flex-1 flex items-center bg-mauve-4 h-10 px-4 gap-x-3 font-bold rounded-xl py-1"
+        className="flex h-10 flex-1 items-center gap-x-3 rounded-xl bg-mauve-4 px-4 py-1 font-bold"
       >
         {/* // TOPページのみ検索アイコンを表示 */}
         {!isSearchPage && (
-          <div className="flex-shrink-0 flex items-center">
-            <Image
-              src="/assets/icons/SearchIcon.svg"
-              alt=""
-              width={20}
-              height={20}
-            />
+          <div className="flex flex-shrink-0 items-center">
+            <Image src="/assets/icons/SearchIcon.svg" alt="" width={20} height={20} />
           </div>
         )}
         <input
-          className="flex-1 bg-transparent outline-none text-mauve-12 placeholder:text-mauve-9"
+          className="flex-1 bg-transparent text-mauve-12 outline-none placeholder:text-mauve-9"
           type="text"
           value={searchTerm}
           placeholder="シェフやレシピを検索"
           onChange={handleSearchChange}
         />
-        <div className="w-5 h-5 flex items-center">
+        <div className="flex h-5 w-5 items-center">
           {/* Debounce中はSpinnerを表示 */}
           {/* Debounce中でなく、検索文字列が空でない場合はクリアボタンを表示 */}
           {isDebouncing ? (
@@ -78,12 +71,7 @@ export const SearchBar: FC<Props> = ({ isSearchPage = false }) => {
                 setSearchTerm('');
               }}
             >
-              <Image
-                src="/assets/icons/CloseIcon.svg"
-                alt=""
-                width={20}
-                height={20}
-              />
+              <Image src="/assets/icons/CloseIcon.svg" alt="" width={20} height={20} />
             </button>
           ) : null}
         </div>
