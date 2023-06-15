d336b68120ca9322b5325db8600719baa0e7e510
Keitoto <keitasekihara@gmail.com>
2023-06-15 16:12:25 +0900

    💄 Added tailwind variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      height: {
+        'footer': '52px',
+      },
+      padding: {
+        'footer': '52px',
+      }
     },
   },
   plugins: [require("tailwindcss-radix-colors")],
