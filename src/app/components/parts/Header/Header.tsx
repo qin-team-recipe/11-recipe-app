0fddabc0cca819b772077dc3803222675243fcff
Keitoto <keitasekihara@gmail.com>
2023-06-15 16:17:19 +0900

    💄 背景削除

--- src/app/components/parts/Header/Header.tsx
+++ src/app/components/parts/Header/Header.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 
 export const Header:FC<{children:ReactNode}> = ({children}) => {
   return (
-    <header className="w-full flex justify-center items-center h-12 border-b border-mauve-6 bg-mauve-2 font-bold text-xl leading-0">
+    <header className="w-full flex justify-center items-center h-12 border-b border-mauve-6 font-bold text-xl leading-0">
       {children}
     </header>
   )
