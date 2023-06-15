9eb2e17fc5527154a23cbf8990313dc2a000d392
Keitoto <keitasekihara@gmail.com>
2023-06-15 17:40:47 +0900

    ✨ HomePageコンテント修正

--- src/app/(pages)/(search)/(home)/page.tsx
+++ src/app/(pages)/(search)/(home)/page.tsx
@@ -1,26 +1,27 @@
-import { RecipeListHorizontal, RecipeListVertical } from '@/app/components/features/recipe/RecipeList';
 import Link from 'next/link';
+
+import { RecipeListHorizontal } from '@/app/components/features/recipe/RecipeList';
 
 export default function Home() {
   return (
-    <div className='space-y-12 pt-5 px-4'>
+    <div className="space-y-12 px-4 pt-5">
       <section>
-        <h2 className='text-xl font-bold mb-4'>話題のシェフ</h2>
+        <h2 className="mb-4 text-xl font-bold">話題のシェフ</h2>
+        <p>TODO: シェフリスト</p>
+      </section>
+      <section>
+        <div className="flex items-center justify-between">
+          <h2 className="mb-4 text-xl font-bold">話題のレシピ</h2>
+          <Link href="/search/recipe">もっと見る</Link>
+        </div>
         <RecipeListHorizontal recipeList={[]} />
       </section>
       <section>
-        <div className="flex justify-between items-center">
-          <h2 className='text-xl font-bold mb-4'>話題のレシピ</h2>
-          <Link href='/search/recipe'>もっと見る</Link>
+        <div className="flex items-center justify-between">
+          <h2 className="mb-4 text-xl font-bold">シェフ</h2>
+          <Link href="/search/chef">もっと見る</Link>
         </div>
-        <RecipeListVertical recipeList={[]} />
-      </section>
-      <section>
-        <div className="flex justify-between items-center">
-          <h2 className='text-xl font-bold mb-4'>シェフ</h2>
-          <Link href='/search/chef'>もっと見る</Link>
-        </div>
-        <RecipeListVertical recipeList={[]} />
+        <p>TODO: シェフリスト</p>
       </section>
     </div>
   );
