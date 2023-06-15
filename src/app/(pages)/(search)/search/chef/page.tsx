0b47499b4f459d26c502b502779d67311c55c41e
Keitoto <keitasekihara@gmail.com>
2023-06-15 19:17:18 +0900

    ✨ 検索結果セクション追加

--- src/app/(pages)/(search)/search/chef/page.tsx
+++ src/app/(pages)/(search)/search/chef/page.tsx
@@ -2,12 +2,23 @@
 
 import { useSearchParams } from 'next/navigation';
 
+import { SearchToggle } from '@/app/components/features/search/SearchToggle/SearchToggle';
+
 const SearchChefPage = () => {
   const searchParams = useSearchParams();
+  const query = searchParams.get('q');
 
-  const search = searchParams.get('q');
+  // TODO:フィルター処理
 
-  return <div>Searching for: {search}</div>;
+  return (
+    <section>
+      <SearchToggle query={query} type="chef" />
+      <h1 className="mb-2 mt-5 px-4 text-xl font-bold">{query ? `「${query}」で検索` : 'シェフ一覧'}</h1>
+      <div className="px-4 pb-2">
+        <p>TODO:ChefList</p>
+      </div>
+    </section>
+  );
 };
 
 export default SearchChefPage;
