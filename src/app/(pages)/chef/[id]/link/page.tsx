2ff4ee2c871595efc2bb586e39cf2ea93c8d1e16
Keitoto <keitasekihara@gmail.com>
2023-06-17 18:19:53 +0900

    ✨ でもサイトを参考に空ページ追加

---
+++ src/app/(pages)/chef/[id]/link/page.tsx
@@ -0,0 +1,4 @@
+const ChefLinkPage = ({ params }: { params: { id: string } }) => {
+  return <div>ChefLinkPage: {params.id}</div>;
+};
+export default ChefLinkPage;
---
+++ src/app/(pages)/new-recipe/page.tsx
@@ -0,0 +1,4 @@
+const NewRecipePage = () => {
+  return <div>NewRecipePage</div>;
+};
+export default NewRecipePage;
