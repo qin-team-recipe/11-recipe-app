2d55c66be9d51e405e6395e5d32ce278a554bc5d
Keitoto <keitasekihara@gmail.com>
2023-06-22 16:57:35 +0900

    ✨ コピー実装

--- src/app/_components/parts/CopyButton/CopyButton.tsx
+++ src/app/_components/parts/CopyButton/CopyButton.tsx
@@ -9,12 +9,26 @@ type Props = {
 };
 
 export const CopyButton: FC<Props> = ({ text }) => {
-  const handleCopy = () => {
-    console.log(text);
+  const copyToClipboard = (tagValue: string) => {
+    if (!navigator.clipboard) {
+      // TODO 非対応時の対応
+      console.log('not supported');
+      return;
+    }
+
+    return navigator.clipboard.writeText(tagValue).then(function () {
+      // TODO 完了メッセージを表示する？
+      console.log('done');
+    });
   };
+
+  const handleCopy = () => {
+    copyToClipboard(text);
+  };
+
   return (
     <div className="px-4 py-2 text-right">
-      <button type="button" onClick={handleCopy} className="inline-flex items-center gap-x-0.5 text-blue-11 text-xs">
+      <button type="button" onClick={handleCopy} className="inline-flex items-center gap-x-0.5 text-xs text-blue-11">
         <IconCopy className="h-4 w-4" />
         コピーする
       </button>
