bb1a9157737b11a54737b2e34c3b4b70aed70e92
Keitoto <keitasekihara@gmail.com>
2023-06-22 16:59:03 +0900

    ✨ 各コンポーネントをページに配置

--- src/app/(pages)/chef/[id]/page.tsx
+++ src/app/(pages)/chef/[id]/page.tsx
@@ -1,9 +1,23 @@
 import { ChefDetailsInfo } from '@/app/_components/features/chef/ChefDetailsInfo/ChefDetailsInfo';
+import { LinkTab } from '@/app/_components/parts/LinkTab';
 import { dummyChefList } from '@/app/_data/dummyChef';
 
 const ChefDetailPage = ({ params }: { params: { id: string } }) => {
   const chef = dummyChefList.filter((item) => item.id === params.id);
   if (!chef) return <div></div>;
+
+  const tabs = [
+    {
+      href: `/chef/${params.id}`,
+      label: 'レシピ',
+      isCurrent: true,
+    },
+    {
+      href: `/chef/${params.id}/link`,
+      label: 'リンク',
+      isCurrent: false,
+    },
+  ];
 
   const { name, profile, imageUrl, numFollows, numRecipes } = chef[0];
 
@@ -16,6 +30,7 @@ const ChefDetailPage = ({ params }: { params: { id: string } }) => {
         numFollows={numFollows}
         numRecipes={numRecipes}
       />
+      <LinkTab tabs={tabs} />
       ChefDetailPage: {params.id}
     </div>
   );
--- src/app/(pages)/recipe/[id]/page.tsx
+++ src/app/(pages)/recipe/[id]/page.tsx
@@ -1,16 +1,32 @@
 import { RecipeDetailsInfo } from '@/app/_components/features/recipe/RecipeDetailsInfo';
+import { StepsList } from '@/app/_components/features/recipe/StepsList';
+import { LinkTab } from '@/app/_components/parts/LinkTab';
 import { dummyRecipeList } from '@/app/_data/dummyRecipe';
 
 const RecipeDetailPage = ({ params }: { params: { id: string } }) => {
   const recipe = dummyRecipeList.filter((item) => item.id === params.id);
   if (!recipe) return <div></div>;
 
-  const { name, overview, recipeImages, numLikes, chef } = recipe[0];
+  const tabs = [
+    {
+      href: `/recipe/${params.id}`,
+      label: '作り方',
+      isCurrent: true,
+    },
+    {
+      href: `/recipe/${params.id}/ingredients`,
+      label: '材料',
+      isCurrent: false,
+    },
+  ];
+
+  const { name, overview, recipeImages, numLikes, chef, recipeSteps } = recipe[0];
 
   return (
     <div>
       <RecipeDetailsInfo name={name} overview={overview} recipeImages={recipeImages} numLikes={numLikes} chef={chef} />
-      RecipeDetailPage: {params.id}
+      <LinkTab tabs={tabs} />
+      <StepsList steps={recipeSteps} />
     </div>
   );
 };
