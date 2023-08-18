1a441189072bf245a5e0d1f9a1da4732cf6f8c50
Keitoto <keitasekihara@gmail.com>
2023-08-18 20:35:58 +0900

    💄 ページ背景色変更

--- src/app/(pages)/list/layout.tsx
+++ src/app/(pages)/list/layout.tsx
@@ -3,10 +3,10 @@ import { PageHead } from '@/app/_components/parts/PageHead';
 
 export default function ListLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className='bg-mauve-2 min-h-screen'>
       <PageHead>買い物リスト</PageHead>
       {children}
       <Footer />
-    </>
+    </div>
   );
 }
